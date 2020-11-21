80d4986f48504d8117acde1b4e9986cedba719d9
Jan Chmelicek <chmeldax@gmail.com>
2020-11-21 15:09:59 +0100

    Create model for property sale
    Model contains only fields relevant for this task.
    From the data standpoint, it would make sense to load
    it into DB with all data, but let's skip this for now.

--- prices/models.py
+++ prices/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class PropertySale(models.Model):
+    PROPERTY_TYPE_CHOICES = (
+        ("D", "Detached"),
+        ("S", "Semi-Detached"),
+        ("T", "Terraced"),
+        ("F", "Flats/Maisonettes"),
+        ("O", "Other"),
+    )
+
+    price = models.IntegerField()
+    date_of_transfer = models.DateTimeField()
+    postcode = models.CharField(max_length=8)
+    property_type = models.CharField(choices=PROPERTY_TYPE_CHOICES, max_length=1)
